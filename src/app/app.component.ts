c3b578713182a4bd7d1aa3b1b74ef763ccdc8b6a
Juan Israel dos Anjos <juaniwk3@gmail.com>
2022-02-15 23:21:43 -0300

    calc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,92 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-calculator';
+  displayValue: string = '';
+  firstNumber: string = '';
+  secondNumber: string = '';
+  waitingNumber: boolean = false;
+  operator: string = '';
+
+  @Input() dataKey: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  numberPress(event: MouseEvent): void {
+    let number = (event.target as Element).getAttribute('data-key');
+
+    if (number !== null) {
+      if (!this.waitingNumber) {
+        this.firstNumber = this.firstNumber + number;
+        this.displayValue = this.displayValue + number;
+      } else {
+        this.secondNumber = this.secondNumber + number;
+        this.displayValue = this.displayValue + number;
+      }
+    }
+  }
+
+  clear() {
+    this.firstNumber = '';
+    this.secondNumber = '';
+    this.displayValue = '';
+  }
+
+  add() {
+    this.displayValue = `${this.firstNumber} + `;
+    this.waitingNumber = true;
+    this.operator = '+';
+  }
+
+  subtract() {
+    this.displayValue = `${this.firstNumber} - `;
+    this.waitingNumber = true;
+    this.operator = '-';
+  }
+  multiply() {
+    this.displayValue = `${this.firstNumber} * `;
+    this.waitingNumber = true;
+    this.operator = '*';
+  }
+  divide() {
+    this.displayValue = `${this.firstNumber} ÷ `;
+    this.waitingNumber = true;
+    this.operator = '÷';
+  }
+
+  equal() {
+    if (this.firstNumber !== '' && this.secondNumber !== '') {
+      switch (this.operator) {
+        case '+':
+          this.displayValue = (
+            parseInt(this.firstNumber) + parseInt(this.secondNumber)
+          ).toString();
+          break;
+        case '-':
+          this.displayValue = (
+            parseInt(this.firstNumber) - parseInt(this.secondNumber)
+          ).toString();
+          break;
+        case '*':
+          this.displayValue = (
+            parseInt(this.firstNumber) * parseInt(this.secondNumber)
+          ).toString();
+          break;
+        case '÷':
+          this.displayValue = (
+            parseInt(this.firstNumber) / parseInt(this.secondNumber)
+          ).toString();
+          break;
+      }
+    }
+    this.firstNumber = this.displayValue;
+    this.secondNumber = '';
+    this.waitingNumber = false;
+  }
 }
